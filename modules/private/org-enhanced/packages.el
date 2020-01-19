b13ef80423d23704283104e90d6160aeed8e78e1
Guoqiang Jin <jinguoqiang@kingsmith.com.cn>
2020-01-19 20:15:35 +0800

    using org-plus-contrib instead of builtin org.

--- modules/private/org-enhanced/packages.el
+++ modules/private/org-enhanced/packages.el
@@ -1,6 +1,26 @@
 ;; -*- no-byte-compile: t; -*-
 
 ;;(package! deft)
+;;;; Prevent built-in Org from playing into the byte-compilation of
+;; `org-plus-contrib'.
+(when-let (orglib (locate-library "org" nil doom--initial-load-path))
+  (setq load-path (delete (substring (file-name-directory orglib) 0 -1)
+                          load-path)))
+
+;; HACK A necessary hack because org requires a compilation step after being
+;;      cloned, and during that compilation a org-version.el is generated with
+;;      these two functions, which return the output of a 'git describe ...'
+;;      call in the repo's root. Of course, this command won't work in a sparse
+;;      clone, and more than that, initiating these compilation step is a
+;;      hassle, so...
+(add-hook! 'straight-use-package-pre-build-functions
+  (defun +org-fix-package-h (package &rest _)
+    (when (member package '("org" "org-plus-contrib"))
+      (with-temp-file (expand-file-name "org-version.el" (straight--repos-dir "org"))
+        (insert "(fset 'org-release (lambda () \"9.3\"))\n"
+                "(fset 'org-git-version #'ignore)\n"
+                "(provide 'org-version)\n")))))
+(package! org-plus-contrib)
 (package! notdeft :recipe (:host github :repo "hasu/notdeft" :branch "xapian"))
 (package! ox-gfm)
 (package! ob-ipython)
