eddccbb780bb7b0815fe22763f342901529a9efb
Guoqiang Jin <ustczhan@gmail.com>
2021-10-11 02:30:33 +0800

    add tree-sitter

---
+++ modules/private/tree-sitter/config.el
@@ -0,0 +1,7 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package! tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
